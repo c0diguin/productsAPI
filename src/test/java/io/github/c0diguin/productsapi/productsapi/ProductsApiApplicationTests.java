7332fdf3582ab9bf76e2121b09ab2da79fc4d957
c0diguin <codiguin@git.com>
2025-08-25 17:47:21 -0300

    Criacao do Projeto

---
+++ src/test/java/io/github/c0diguin/productsapi/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.c0diguin.productsapi.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
