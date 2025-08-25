05638884d77f67f86dfbca0865b70a75e5847864
c0diguin <codiguin@git.com>
2025-08-25 20:01:12 -0300

    Create getProductById method

--- src/main/java/io/github/c0diguin/productsapi/productsapi/controller/ProductController.java
+++ src/main/java/io/github/c0diguin/productsapi/productsapi/controller/ProductController.java
@@ -2,10 +2,7 @@ package io.github.c0diguin.productsapi.productsapi.controller;
 
 import io.github.c0diguin.productsapi.productsapi.model.Product;
 import io.github.c0diguin.productsapi.productsapi.repository.ProductRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,5 +25,8 @@ public class ProductController {
         return product;
     }
 
-    
+    @GetMapping("/{id}")
+    public Product getProductByID (@PathVariable("id") String id) {
+        return productRepository.findById(id).orElse(null);
+    }
 }
