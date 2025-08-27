15f048daa93598082848063bf36bd42a2402d2e5
c0diguin <codiguin@git.com>
2025-08-27 12:57:45 -0300

    Create update and findProductByName method

--- src/main/java/io/github/c0diguin/productsapi/productsapi/controller/ProductController.java
+++ src/main/java/io/github/c0diguin/productsapi/productsapi/controller/ProductController.java
@@ -4,6 +4,7 @@ import io.github.c0diguin.productsapi.productsapi.model.Product;
 import io.github.c0diguin.productsapi.productsapi.repository.ProductRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -33,5 +34,16 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public void deleteProductByID (@PathVariable("id") String id) {
         productRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void update (@PathVariable("id") String id, @RequestBody Product product) {
+        product.setId(id);
+        productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> findProductByName (@RequestParam(name = "name") String name) {
+        return productRepository.findByName(name);
     }
 }
--- src/main/java/io/github/c0diguin/productsapi/productsapi/repository/ProductRepository.java
+++ src/main/java/io/github/c0diguin/productsapi/productsapi/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package io.github.c0diguin.productsapi.productsapi.repository;
 import io.github.c0diguin.productsapi.productsapi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+    
+    List<Product>findByName(String name);
 }
